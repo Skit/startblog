5a313bf53aa52da3007893d2f1416a2ade7f0298
Skit <wabto@mail.ru>
2014-10-17 21:53:08 +0800

    install easyimage extension

---
+++ protected/config/main.php
@@ -0,0 +1,100 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'My Web Application',
+
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	// autoloading model and component classes
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+        'ext.easyimage.EasyImage',
+	),
+    'defaultController'=>'post',
+
+	'modules'=>array(
+		'gii'=>array(
+			'class'=>'system.gii.GiiModule',
+			'password'=>'111',
+			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'ipFilters'=>array('127.0.0.1','::1'),
+		),
+	),
+
+	// application components
+	'components'=>array(
+		'user'=>array(
+			// enable cookie-based authentication
+			'allowAutoLogin'=>true,
+		),
+        'log'=>array(
+            'class'=>'CLogRouter',
+            'routes'=>array(
+                array(
+                    'class'=>'CFileLogRoute',
+                    'levels'=>'error, warning',
+                ),
+            ),
+        ),
+		// uncomment the following to enable URLs in path-format
+
+            'urlManager'=>array(
+            'urlFormat'=>'path',
+            'rules'=>array(
+                'post/<id:\d+>/<title:.*?>'=>'post/view',
+                'posts/<tag:.*?>'=>'post/index',
+                'post/update/<id:\d+>'=>'post/update',
+                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+            ),
+		),
+		/*'db'=>array(
+			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+		),*/
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=startblog',
+			'emulatePrepare' => true,
+			'username' => 'root',
+			'password' => '',
+			'charset' => 'utf8',
+            'tablePrefix' => 'tbl_',
+		),
+		'errorHandler'=>array(
+			// use 'site/error' action to display errors
+			'errorAction'=>'site/error',
+		),
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+				// uncomment the following to show log messages on web pages
+				/*
+				array(
+					'class'=>'CWebLogRoute',
+				),
+				*/
+			),
+		),
+        'easyImage' => array(
+            'class' => 'application.extensions.easyimage.EasyImage',
+            //'driver' => 'GD',
+            //'quality' => 100,
+            //'cachePath' => '/assets/easyimage/',
+            //'cacheTime' => 2592000,
+            //'retinaSupport' => false,
+        ),
+	),
+    // application-level parameters that can be accessed
+    // using Yii::app()->params['paramName']
+    'params'=>require(dirname(__FILE__).'/params.php'),
+);
